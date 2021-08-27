3528a0c86ea70bebcb23b5e8d7bb559120b414d5
miumio <nickolskaya@gmail.com>
2021-08-27 17:29:07 +0300

    🙈

--- build/js/main.js
+++ build/js/main.js
@@ -1,20 +1,22 @@
-const footer = document.querySelector('.footer');
-const menuLink = footer.querySelectorAll('h3');
-const menuList = footer.querySelectorAll('.footer__list');
+(function () {
+  const footer = document.querySelector('.footer');
+  const menuLink = footer.querySelectorAll('footer__accordion-link');
+  const menuList = footer.querySelectorAll('.footer__list');
 
-menuList.forEach(function (item) {
-  item.classList.add('footer__list--hidden');
-})
-
-menuLink.forEach(function (item) {
-  item.classList.add('footer__acc-link--active');
-})
-
-menuLink.forEach(function (el) {
-
-  el.addEventListener('click', function () {
-
-    el.classList.toggle('footer__acc-link--active');
-    el.nextElementSibling.classList.toggle('footer__list--hidden');
+  menuList.forEach(function (item) {
+    item.classList.add('footer__list--hidden');
   })
-})
+
+  menuLink.forEach(function (item) {
+    item.classList.add('footer__acc-link--active');
+  })
+
+  menuLink.forEach(function (item) {
+
+    item.addEventListener('click', function () {
+
+      item.classList.toggle('footer__acc-link--active');
+      item.nextElementSibling.classList.toggle('footer__list--hidden');
+    });
+  });
+})();
--- build/js/nav.js
+++
@@ -1,7 +0,0 @@
-(function () {
-
-  const footer = document.querySelector('.footer');
-  const menuBtn = footer.querySelectorAll('h3');
-  const menuList = footer.querySelectorAll('.footer_list');
-
-})();
